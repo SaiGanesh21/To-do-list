3567c21c523685460d2a7ba51704374a24b5fcc9
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2022-07-24 02:42:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,29 @@
-const addForm = document.querySelector(".add");
-const list = document.querySelector(".todos");
-const search = document.querySelector(".search input");
-
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
 
 const generateTemplate = todo => {
+   
+   const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+    </li>`;
 
-  const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
-  <span>${todo}</span>
-  <i class="fas fa-trash delete"></i>
-</li> `;
-
-  list.innerHTML += html;
-
-};
+    list.innerHTML += html;
+}
 
 addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+   
+    if (todo.length) {
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
 
-  e.preventDefault();
-  const todo = addForm.add.value.trim();
-
-  if (todo.length) {
-    generateTemplate(todo);
-    addForm.reset();
-  };
-
-
+//delete todos
+list.addEventListener('click', e=> {
+    if(e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
 })
-
-list.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
-  };
-
-});
-
-
-const filterToDos = (input) => {
-
-  console.log(Array.from(list.children));
-  
-  Array.from(list.children)
-   .filter((todo)=> !todo.textContent.includes(input))
-   .forEach((todo) =>todo.classList.add('filtered'));
-
-   Array.from(list.children)
-   .filter((todo) =>todo.textContent.includes(input))
-   .forEach((todo)=> todo.classList.remove('filtered'));
-  
-  };
-search.addEventListener('keyup',() => {
-  const input = search.value.trim();
-  filterToDos(input);
-  
-});
