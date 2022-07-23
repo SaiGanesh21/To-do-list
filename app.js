cc5b100cec948507e23d03eb8314d06b94f6edbb
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2022-07-24 03:10:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,104 +1,62 @@
-// delete a book
+const form = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
-const list =  document.querySelector('#book-list ul');
+const makeTemplate = todo => {
+  //generate template and output it
 
-list.addEventListener('click', (e)=>{
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li);
-    }
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="fas fa-trash-alt delete"></i>
+    </li>
+  `;
+  //now place into ul
+  list.innerHTML += html;
+
+};
+
+
+form.addEventListener('submit', e => {
+
+  e.preventDefault();
+  const todo = form.add.value.trim();//trim method removes white space before or after string
+  //console.log(todo); //checking to confirm everything works
+  if(todo.length){//true or false (0 todo.length is false, if statement won't fire)
+    makeTemplate(todo);
+    form.reset();//reset input field inside that form
+  };  
+
 });
 
-// add a book
+//delete todos
+list.addEventListener('click', e => {
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  };
+})
 
-const addForm = document.forms['add-book'];
 
-addForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const input = addForm.querySelector("input[type='text']");
-    const value = addForm.querySelector("input[type='text']").value;
-    
-    if (value){
-        //create elements
-        const li = document.createElement('li');
-        const bookName = document.createElement('span');
-        const deleteBtn = document.createElement('span');
-        
-        //add classes
-        bookName.classList.add('name');
-        deleteBtn.classList.add('delete');
-    
-        ///add content
-        deleteBtn.textContent = 'delete';
-        bookName.textContent = value;
-    
-        //append to document, order matters
-        li.appendChild(bookName);
-        li.appendChild(deleteBtn);
-        list.appendChild(li); 
-        
-        //set value to ''
-        input.value='';
+//includes only todos witch don't include the term
+//adds and removes filtered class based on what comes from search event listener
+const filterTodos = (searchTerm) => {
+ 
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(searchTerm))
+    //textContent looks as all text inside li tag
+    //! negates return todo to false
+    .forEach((todo) => todo.classList.add('filtered'));
 
-        //remove red border and <p>
-        input.style.border = '';
-        const p = input.parentElement.querySelector('p');
-        input.parentElement.removeChild(p);
-    } else {
-        if(!addForm.querySelector('p')){
-            input.style.border = 'red 2px solid';
-            addForm.innerHTML += "<p>Task name is required.</p>"
-        }
-    }
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(searchTerm))
+    .forEach((todo) => todo.classList.remove('filtered'));
+  
+
+};
+
+
+//keyup event
+search.addEventListener('keyup', () => {
+  const searchTerm = search.value.trim().toLowerCase();
+  filterTodos(searchTerm);
 });
-
-
-//hide books
-const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', (e)=>{
-    if(hideBox.checked){
-        list.style.display = 'none';
-    }else{
-        list.style.display = 'initial';
-    }
-})
-
-//filter books
-
-const searchBar = document.forms['search-books'].querySelector('input');
-searchBar.addEventListener('keyup', (e)=>{
-    const term = e.target.value.toLowerCase();
-    const books = list.getElementsByTagName('li');
-    Array.from(books).forEach((book)=>{
-        const title = book.firstElementChild.textContent;
-        if(title.toLowerCase().indexOf(term) >= 0 ){
-            book.style.display = 'block';
-        } else {
-            book.style.display = 'none';
-        }
-    });
-})
-
-//tabbed content
-
-const tabs = document.querySelector('.tabs');
-const panels = document.querySelectorAll('.panel');
-
-tabs.addEventListener('click', (e)=>{
-    console.log(e);
-    
-    if(e.target.tagName == 'LI') {
-        const targetPanel = document.querySelector(e.target.dataset.target);
-        panels.forEach((panel)=>{
-            if(panel == targetPanel){
-                panel.classList.add('active');
-            } else {
-                panel.classList.remove('active');
-            }
-        })
-    }
-});
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    //do something....
-})
